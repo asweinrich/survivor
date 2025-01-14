35452b87f8bd32717ddbda39e1ab0df29d15da4c
Andrew Weinrich <asweinrich@gmail.com>
2025-01-14 12:14:37 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>{metadata.title}</title>
+        <title>{String(metadata.title)}</title>
         <meta name="description" content={metadata.description} />
       </head>
       <body

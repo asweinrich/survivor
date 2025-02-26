65c35352328f7e94eedb9be1621d22e5c9267aac
Andrew Weinrich <asweinrich@gmail.com>
2025-02-26 13:28:44 -0800

    Update page.tsx

--- src/app/draft/page.tsx
+++ src/app/draft/page.tsx
@@ -58,7 +58,7 @@ export default function Draft() {
   const season = 48;
 
   //disable draft until it opens
-  const status = true;
+  const status = false;
 
   const [emojiPickerVisible, setEmojiPickerVisible] = useState(false);
 
